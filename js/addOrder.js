36e2218ecb2e6a74d39b5f888f2d0474dcabe669
Brian <mulyadi.brian@gmail.com>
2016-06-08 12:24:32 +0700

    create auth system

--- js/addOrder.js
+++ js/addOrder.js
@@ -40,8 +40,10 @@ $(document).ready(function(){
     var $toAddress = $('#toAddress');
     var $itemDesc = $('#itemDesc');
     var $addOrder = $('#add-order');
+    var $clientId = "EawDasGgVcMWh4s4GCeiItmIVtU2";
 
     var newOrder = {
+        "client_id": $clientId,
         "order_id": $.now(),
         "date": $formatDate,
         "from": {
@@ -75,10 +77,27 @@ $(document).ready(function(){
         type: 'POST',
         url: 'https://lady-delivery.firebaseio.com/order.json',
         contentType: "application/json; charset=utf-8",
-        data: JSON.stringify(newOrder), // no need for JSON.stringify here
+        data: JSON.stringify(newOrder),
         success: function(data) {
             console.log("Order added!", data);
+        },
+        error: function (jqXHR) {
+            if (jqXHR.status == 401) {
+                alert("401: Authentication Error!");
+            } else if (jqXHR.status == 400) {
+                alert("400: Invalid data format in input");
+            };
         }
+    });
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://hooks.zapier.com/hooks/catch/1402225/upcokh/',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(newOrder),
+        success: function(data) {
+            console.log("Order added!", data);
+        },
     });
 });
 });
